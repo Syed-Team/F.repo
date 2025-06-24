0a7eda726c1cea8fefd703ff325692165984ffc1
syed Git <syed@git.com>
2025-06-24 14:34:36 +0530

    Add Flask app and update requirements

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
-print('Hello from CI'Syed,(1).odt')
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello Syed! Your app is deployed successfully."
+
+if __name__ == "__main__":
+    app.run()
